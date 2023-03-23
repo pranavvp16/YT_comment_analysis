91c4ec462b36c6f4e79ce2b43fbe415788d0b32c
Pranav <pranavprajapati586@gmail.com>
2023-03-23 18:07:26 +0530

    Wrote doc string for class and methods, to provide info while hovering or instantiating

--- Comment_scrapper.py
+++ Comment_scrapper.py
@@ -4,21 +4,33 @@ import time
 import os
 import numpy as np
 
+options = webdriver.ChromeOptions
+options.add_argument("--headless")
+options.add_argument("--mute-audio")
+options.add_argument("-no-sandbox")
+options.add_argument("--disable-extension")
+options.add_argument("-disable-dev-shm-usage")
 
-
-class comment_scapper(webdriver.Chrome):
+class youtube_scapper(webdriver.Chrome(options=options)):
     """
-    This is a youtube comment scrapper made with selenium to scrap comments given a URL
-    Parameters
-    video_url : (string)
+        This is a youtube scrapper is made with selenium to scrap different elements
+        on the youtube web-page given a URL
+        Parameters
+        video_url : (string)
     """
     def __init__(self,driver_path="r/selenium_driver/chromedriver_mac64/chromedriver",video_url="string"):
         self.driver_path = driver_path
         self.video_url = video_url
         os.environ['PATH'] += self.driver_path
-        super(comment_scapper, self).__init__()
+        super(youtube_scapper, self).__init__()
 
     def yt_comments(self,convert_to_csv=False):
+        """
+            Scraps youtube comments
+            Parameters:
+                convert_to_csv = False (default)
+                Set to True to get output in a form of .csv file
+        """
         self.get(self.video_url)
         while True:
             self.execute_script("window.scrollTo(0, document.documentElement.scrollHeight);")
