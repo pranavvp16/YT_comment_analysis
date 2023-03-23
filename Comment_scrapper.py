1878613bdca32fcad4e07d3baa7981451f3d2660
Pranav <pranavprajapati586@gmail.com>
2023-03-23 16:42:37 +0530

    added the method of scrapping comments in the scrapper

--- Comment_scrapper.py
+++ Comment_scrapper.py
@@ -12,8 +12,23 @@ class Comment_scapper(webdriver.Chrome):
     def __init__(self,driver_path="r/selenium_driver/chromedriver_mac64/chromedriver",video_url="string"):
         self.driver_path = driver_path
         self.video_url = video_url
+        os.environ['PATH'] += self.driver_path
         super(Comment_scapper, self).__init__()
 
-    def yt_page(self):
+    def yt_comments(self):
         self.get(self.video_url)
+        while True:
+            self.execute_script("window.scrollTo(0, document.documentElement.scrollHeight);")
+            time.sleep(2)
+            new_height = driver.execute_script("return document.documentElement.scrollHeight")
+            if new_height > 3000:
+                break
+        comments = driver.find_elements(By.XPATH, '//*[@id="content-text"]')
+        comments_list = [i for i in comments]
+        return comments_list
+
+
+
+
+
 
