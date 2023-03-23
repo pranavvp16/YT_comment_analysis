5747ea23a3a3e67de74a6616f06d6f66d3cf36f3
Pranav <pranavprajapati586@gmail.com>
2023-03-23 17:11:07 +0530

    fixed the .text method returned from selenium object

--- Comment_scrapper.py
+++ Comment_scrapper.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 import time
 import os
 
-class Comment_scapper(webdriver.Chrome):
+class comment_scapper(webdriver.Chrome):
     """
     This is a youtube comment scrapper made with selenium to scrap comments given a URL
     Parameters
@@ -13,18 +13,20 @@ class Comment_scapper(webdriver.Chrome):
         self.driver_path = driver_path
         self.video_url = video_url
         os.environ['PATH'] += self.driver_path
-        super(Comment_scapper, self).__init__()
+        super(comment_scapper, self).__init__()
 
     def yt_comments(self):
         self.get(self.video_url)
         while True:
             self.execute_script("window.scrollTo(0, document.documentElement.scrollHeight);")
             time.sleep(2)
-            new_height = driver.execute_script("return document.documentElement.scrollHeight")
+            new_height = self.execute_script("return document.documentElement.scrollHeight")
             if new_height > 3000:
                 break
-        comments = driver.find_elements(By.XPATH, '//*[@id="content-text"]')
-        comments_list = [i for i in comments]
+        comments = self.find_elements(By.XPATH, '//*[@id="content-text"]')
+        comments_list =[]
+        for i in comments:
+            comments_list.append(i.text)
         return comments_list
 
 
