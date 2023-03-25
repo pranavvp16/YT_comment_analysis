08db57086e9e443ba5ba248ae9d3a3c98e910e58
Pranav <pranavprajapati586@gmail.com>
2023-03-25 11:11:36 +0530

    Fixed Attribute error in the pipeline function

--- helper.py
+++ helper.py
@@ -6,9 +6,9 @@ import pandas as pd
 def sentiment(video_url):
     with youtube_scrapper(video_url=video_url) as scrapper:
         comments = scrapper.yt_comments()
-        comments = pd.Series(comments)
         sentiment_analysis = pipeline("sentiment-analysis")
         comment_analysis = sentiment_analysis(comments)
         df = pd.DataFrame.from_dict(comment_analysis)
+        comments = pd.Series(comments)
         df["comments"] = comments
     return df
