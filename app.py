adde87461355ac7d6e4ffa59fd1bcb7aa7ef78a8
Pranav <pranavprajapati586@gmail.com>
2023-03-25 16:17:34 +0530

    added to filter options to view filtered dataframe

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 import streamlit as st
 from helper import sentiment
+import matplotlib.pyplot as plt
 
 st.sidebar.title("Comment Analytics")
 video_url = st.sidebar.text_input("Enter Video URL of the video for analysis")
+
 if "https://youtu.be" not in video_url:
     st.sidebar.write("Enter video URL")
 else:
-    sentiment(video_url)
+    df, title = sentiment(video_url)
+    st.title(title)
+    filter_sentiment = st.sidebar.selectbox("Filter", ["Overall", "POSITIVE", "NEGATIVE"])
+    if filter_sentiment != "Overall":
+        filtered_df = df[df["label"] == filter_sentiment]
+        st.dataframe(filtered_df)
+    else:
+        st.dataframe(df)
