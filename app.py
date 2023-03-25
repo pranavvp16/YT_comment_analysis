c22bce7b9933612d6e8ee6d9471751963197b393
Pranav <pranavprajapati586@gmail.com>
2023-03-25 11:02:04 +0530

    added the input text and validated the string in the input

--- app.py
+++ app.py
@@ -1,2 +1,9 @@
 import streamlit as st
+from helper import sentiment
 
+st.sidebar.title("Comment Analytics")
+video_url = st.sidebar.text_input("Enter Video URL of the video for analysis")
+if "https://youtu.be" not in video_url:
+    st.sidebar.write("Enter video URL")
+else:
+    sentiment(video_url)
