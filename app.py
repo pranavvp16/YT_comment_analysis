3344cd00e5b102664e10772bf21bd86052a48d4b
Pranav <pranavprajapati586@gmail.com>
2023-04-28 01:53:44 +0530

    Show the question Dataframe too

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ import matplotlib.pyplot as plt
 
 
 @st.cache_data()
-def dataframe(url):
-    data, video_title = sentiment(video_url)
-    return data, video_title
+def dataframe(video_url):
+    data_q, data_f, video_title = sentiment(video_url)
+    return data_q, data_f, video_title
 
 
 st.sidebar.title("Comment Analytics")
@@ -16,19 +16,21 @@ video_url = st.sidebar.text_input("Enter Video URL of the video for analysis")
 if "https://youtu.be" not in video_url:
     st.sidebar.write("Enter video URL")
 else:
-    df, title = dataframe(video_url)
+    df_feedbacks, df_questions, title = dataframe(video_url)
     st.title(title)
     filter_sentiment = st.sidebar.selectbox("Filter", ["Overall", "POSITIVE", "NEGATIVE"])
     if filter_sentiment != "Overall":
-        filtered_df = df[df["label"] == filter_sentiment]
+        filtered_df = df_feedbacks[df_feedbacks["label"] == filter_sentiment]
         st.dataframe(filtered_df)
+        st.dataframe(df_questions)
     else:
-        st.dataframe(df)
+        st.dataframe(df_feedbacks)
+        st.dataframe(df_questions)
 
     fig, ax = plt.subplots()
     fig1, ax1 = plt.subplots()
     col1, col2 = st.columns(2)
-    label, numbers = helper.plot_bar(df)
+    label, numbers = helper.plot_bar(df_feedbacks)
     with col1:
         ax.bar(label, numbers, color="red")
         st.pyplot(fig)
@@ -37,7 +39,7 @@ else:
                 wedgeprops={'edgecolor': 'black', 'linewidth': 2})
         st.pyplot(fig1)
 
-    df_wc = helper.wc_gen(df)
+    df_wc = helper.wc_gen(df_feedbacks)
     fig, ax = plt.subplots()
     ax.imshow(df_wc)
     st.pyplot(fig)
